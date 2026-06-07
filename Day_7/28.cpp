e81fc76815cde663ae507e1af7c7cb345ffb8792
Tejas Singh <tejassingh04444@gmail.com>
2026-06-07 11:56:29 +0530

    Day_7 uploaded.

---
+++ Day_7/28.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int reverse(int n, int rev = 0)
+{
+    if (n == 0)
+    {
+        return rev;
+    }
+
+    return reverse(n / 10, rev * 10 + n % 10);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number :- ";
+    cin >> n;
+
+    cout << "Reversed number = " << reverse(n);
+
+    return 0;
+}
